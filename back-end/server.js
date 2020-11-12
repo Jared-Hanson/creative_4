49f415c649232d7f88e71f3717bb7df172eb4dad
Jared Hanson <jrodhanson@gmail.com>
2020-11-12 16:30:04 -0700

    first

--- back-end/server.js
+++ back-end/server.js
@@ -13,6 +13,78 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/museum', {
   useNewUrlParser: true
 });
-
-
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+const itemSchema = new mongoose.Schema({
+  title: String,
+  discription: String,
+  path: String,
+});
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+// Create a model for items in the museum.
+const Item = mongoose.model('Item', itemSchema);
+app.post('/api/items', async (req, res) => {
+  const item = new Item({
+    title: req.body.title,
+    discription: req.body.discription,
+    path: req.body.path,
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+app.get('/api/items', async (req, res) => {
+  try {
+    let items = await Item.find();
+    res.send(items);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+app.delete('/api/items/:id', async (req, res) => {
+  try {
+    await Item.deleteOne({
+      _id: req.params.id
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+app.put('/api/items/:id', async (req, res) => {
+  try {
+    item = await Item.findOne({
+      _id: req.params.id
+    });
+    console.log(item.title)
+    console.log(req.body.title)
+    item.title = req.body.title
+    item.discription = req.body.discription
+    item.save()
+    console.log(item.title)
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 app.listen(3000, () => console.log('Server listening on port 3000!'));
